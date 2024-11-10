9142f6d6949f25775ecff698b7de11535edc0f46
Faiz Rahiemy <frachiemy@gmail.com>
2024-11-11 01:01:05 +0700

    Add surat navigation bar

--- py_cari_ayat/surat/routes.py
+++ py_cari_ayat/surat/routes.py
@@ -17,6 +17,16 @@ def surats():
 @router_surat.route('/<id>') 
 def surat(id: int):
     surat: Surat | None = Surat.query.get(id)
+    prev_surat: int = int(id) - 1
+    if int(id) == 78:
+        prev_surat = 4
+    elif int(id) == 1:
+        prev_surat = 114
+    next_surat: int = int(id) + 1
+    if int(id) == 4:
+        next_surat = 78
+    elif int(id) == 114:
+        next_surat = 1
     if surat is None:
         return redirect(url_for('router_surat.surats', notfound=True))
     return render_template(
@@ -24,5 +34,7 @@ def surat(id: int):
         request=request,
         title='Cari Ayat',
         description='Cari Ayat is a Quran verse search engine by translating Indonesian-spell-latin query to Arabic transliteration using Soundex similarity algorithm, while the comparison algorithm is cosine similarity.',
-        surat = surat
+        surat = surat,
+        prev_surat=prev_surat,
+        next_surat=next_surat
     )

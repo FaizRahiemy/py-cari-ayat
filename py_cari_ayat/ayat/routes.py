dfaaf60d8e97315ea41f05dd61b0e04b7413d842
Faiz Rahiemy <frachiemy@gmail.com>
2024-11-10 23:29:58 +0700

    Show query in templates

--- py_cari_ayat/ayat/routes.py
+++ py_cari_ayat/ayat/routes.py
@@ -1,22 +1,17 @@
 from flask import render_template, request
 
-from py_cari_ayat.ayat import router_ayat
+from py_cari_ayat.ayat import router_ayat, cosine_similarity
+from py_cari_ayat.models.kata import Kata
 
 @router_ayat.route('/query/<query_str>') 
 def query(query_str: str):
+    query_katas: list[Kata] = [Kata(query) for query in query_str.split()]
+    results: list[Kata] = cosine_similarity.get_similar(query_str)
     return render_template(
         'ayat/query.html.j2',
         request=request,
         title="Cari Ayat",
         description="Cari Ayat is a Quran verse search engine by translating Indonesian-spell-latin query to Arabic transliteration using Soundex similarity algorithm, while the comparison algorithm is cosine similarity.",
-        query_str=query_str
-    )
-
-@router_ayat.route('/{id}') 
-def ayat():
-    return render_template(
-        'surat/surat.html.j2',
-        request=request,
-        title="Cari Ayat",
-        description="Cari Ayat is a Quran verse search engine by translating Indonesian-spell-latin query to Arabic transliteration using Soundex similarity algorithm, while the comparison algorithm is cosine similarity."
+        query_katas=query_katas,
+        results=results
     )

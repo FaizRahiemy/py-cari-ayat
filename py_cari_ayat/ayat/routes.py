49e6a20cbf3dd1df7813b88212dadb35824b6df6
Faiz Rahiemy <frachiemy@gmail.com>
2024-11-11 15:22:22 +0700

    Add ayat page, link page from other pages

--- py_cari_ayat/ayat/routes.py
+++ py_cari_ayat/ayat/routes.py
@@ -1,6 +1,7 @@
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for
 
 from py_cari_ayat.ayat import router_ayat, cosine_similarity
+from py_cari_ayat.models.ayat import Ayat
 from py_cari_ayat.models.kata import Kata
 
 @router_ayat.route('/query/<query_str>') 
@@ -10,8 +11,29 @@ def query(query_str: str):
     return render_template(
         'ayat/query.html.j2',
         request=request,
-        title="Cari Ayat",
-        description="Cari Ayat is a Quran verse search engine by translating Indonesian-spell-latin query to Arabic transliteration using Soundex similarity algorithm, while the comparison algorithm is cosine similarity.",
+        title=f'Cari Ayat - {query_str}',
+        description='Cari Ayat is a Quran verse search engine by translating Indonesian-spell-latin query to Arabic transliteration using Soundex similarity algorithm, while the comparison algorithm is cosine similarity.',
         query_katas=query_katas,
         results=results
+    )
+
+@router_ayat.route('/<id>') 
+def ayat(id: int):
+    ayat: Ayat|None = Ayat.query.get(id)
+    prev_ayat: int = int(id) - 1
+    if int(id) == 1:
+        prev_ayat = 1204
+    next_ayat: int = int(id) + 1
+    if int(id) == 1204:
+        next_ayat = 1
+    if ayat is None:
+        return redirect(url_for('router_surat.surats', notfound=True))
+    return render_template(
+        'ayat/ayat.html.j2',
+        request=request,
+        title=f'Cari Ayat - QS:{ayat.surat_id}-{ayat.ayat}',
+        description='Cari Ayat is a Quran verse search engine by translating Indonesian-spell-latin query to Arabic transliteration using Soundex similarity algorithm, while the comparison algorithm is cosine similarity.',
+        ayat=ayat,
+        prev_ayat=prev_ayat,
+        next_ayat=next_ayat
     )
